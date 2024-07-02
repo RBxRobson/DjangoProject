5a43550659da244ef75cfbc91a6149d08caf1a6b
RBxRobson <robsonnbarbosa0@gmail.com>
2024-07-02 15:52:23 -0300

    feat: Adiciona nova url 'home' da aplicação blog

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for django_project project.
+# Importa o módulo admin do Django, que fornece a interface de administração do site
+from django.contrib import admin  
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+# Importa as funções include e path do Django, usadas para definir e incluir URLs
+from django.urls import include, path  
 
+# Define a lista de padrões de URL para o projeto
 urlpatterns = [
+    # Mapeia a URL 'admin/' para a interface de administração do Django
     path('admin/', admin.site.urls),
+    
+    # Inclui as URLs definidas no módulo blog.urls sob o caminho 'home'
+    # Isso significa que qualquer URL que comece com 'home' será tratada pelo módulo blog.urls
+    path('home', include('blog.urls'))
 ]
