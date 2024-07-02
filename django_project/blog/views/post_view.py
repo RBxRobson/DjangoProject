880a145c99c04a4e9181e7bd56b094b02a972254
RBxRobson <robsonnbarbosa0@gmail.com>
2024-07-02 15:49:58 -0300

    feat: Adiciona e exporta a classe de visualização PostView

---
+++ django_project/blog/views/post_view.py
@@ -0,0 +1,13 @@
+# Importa a classe HttpResponse do Django, usada para retornar respostas HTTP simples
+from django.http import HttpResponse  
+
+# Importa a classe base generic do módulo de views genéricas do Django
+from django.views import generic  
+
+
+# Define uma classe de visualização baseada em classe que herda de generic.View
+class PostView(generic.View):
+    # Define um método GET que será chamado quando uma requisição GET for feita a esta view
+    def get(self, request, *args, **kwargs):
+        # Retorna uma resposta HTTP simples com o texto 'Hello World'
+        return HttpResponse('Hello World')
